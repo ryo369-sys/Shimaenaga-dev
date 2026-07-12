6eb60df40315f7d950d5512873ba2bf760986972
ryo369-sys <shimuraryo117@gmail.com>
2026-07-12 18:28:39 +0900

    新規作成と遷移完了

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,17 @@
 import { memo } from "react";
 import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 function Home() {
+
+  const { age } = useParams<{ age: string }>();
+
   return (
-    <nav>
-      <Link to="/Home">home</Link>
-      <p></p>
-    </nav>
+    <div style={{ padding: '20px' }}>
+      <h2>ダッシュボード</h2>
+      <p>新規作成に成功した人だけが見られる秘密のページです！</p>
+      <p>ようこそ、 <strong>{age}</strong> さん！</p>
+    </div>
   );
 }
 
